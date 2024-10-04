4beb3a0fd6f11ebf6cf2fdf59397d6e8e72bfea3
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-10-04 19:15:30 +0200

    Create LLClientHandler

---
+++ src/main/java/de/einkampflama/LLamaNet/api/socketserver/LLClientHandler.java
@@ -0,0 +1,22 @@
+package de.einkampflama.LLamaNet.api.socketserver;
+
+import java.net.Socket;
+
+/**
+ * @author Xyonox
+ * @version 1.0.0
+ * @since alpha-1.0.0
+ */
+public interface LLClientHandler {
+    /**
+     * @implNote don't forget to close the socket
+     * @param clientSocket
+     */
+    void handle(Socket clientSocket);
+
+    default void synchronizedHandle(Socket clientSocket) {
+        synchronized(this) {
+            handle(clientSocket);
+        }
+    }
+}
