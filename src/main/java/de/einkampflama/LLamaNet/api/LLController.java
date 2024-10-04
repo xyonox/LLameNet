97c9d6f0680fc13b8e46996b25fcb262d2f6cad2
einKampfLama <93388182+einKampfLama@users.noreply.github.com>
2024-10-04 19:15:14 +0200

    Move LLController

--- src/main/java/de/einkampflama/LLamaNet/api/LLController.java
+++ src/main/java/de/einkampflama/LLamaNet/api/LLController.java
@@ -1,5 +1,10 @@
-package de.einkampflama.LLamaNet.api.socketserver;
+package de.einkampflama.LLamaNet.api;
 
+/**
+ * @author Xyonox
+ * @version 1.0.0
+ * @since alpha-1.0.0
+ */
 public interface LLController {
     public void start(String time);
     public void stop(String time);
